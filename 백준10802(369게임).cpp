f1a89463421306a31346ecabfcfca55a38523a8c
yoodongmin98 <vpt515@gmail.com>
2024-02-05 13:53:57 +0900

    유동민 : 백준10802 시도중

---
+++ 백준10802(369게임).cpp
@@ -0,0 +1,25 @@
+//#include <iostream>
+//
+//
+//
+//
+//
+//
+//
+//int memo[100010][3];
+//void Solution()
+//{
+//    memo[0][0] = memo[0][2] = 1;
+//    memo[0][1] = 0;
+//    for (int i = 1; i < 100001; i++) {
+//        memo[i][0] = memo[i - 1][0] + memo[i - 1][1] * 6 + memo[i - 1][2] * 3;
+//        memo[i][2] = memo[i - 1][2] * 10;
+//        memo[i][1] = (memo[i - 1][0] + memo[i - 1][1] + memo[i - 1][2]) * 3 + memo[i - 1][1];
+//    }
+//    int a = 0;
+//}
+//
+//int main()
+//{
+//    Solution();
+//}
