dc7e7a98ef59b5efa77baab123ddd41b02e5a6cd
yoodongmin98 <vpt515@gmail.com>
2024-02-07 16:56:05 +0900

    유동민 : Main Parameter Update

---
+++ main parameter.cpp
@@ -0,0 +1,14 @@
+//#include <iostream>
+//
+//
+//
+//
+//using namespace std;
+//
+//int main(int argc,char** argv)
+//{
+//	cout << "argc에는 뭐가 들었나 : " << argc << endl;
+//	for(auto i=0; i<argc; ++i)
+//		cout << "argv에는 뭐가 들었나 : " << argv[i] << endl;
+//}
+//
