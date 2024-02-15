cf0c23d9745bd940e342efbbed034de35f59c1a2
동민 유 <vpt515@gmail.com>
2024-02-15 19:15:42 +0900

    유동민 : 백준 1937 Update

--- 백준 1937번(욕심쟁이 판다).cpp
+++ 백준 1937번(욕심쟁이 판다).cpp
@@ -1,7 +1,7 @@
 //#include <iostream>
 //
 //using namespace std;
-////개 ㅈ같은 문제로구만
+//개 ㅈ같은 문제로구만
 //int N;
 //int Max = 0;
 //int Map[501][501];
@@ -12,20 +12,24 @@
 //{
 //	bool SideCheck = false;
 //	int Now = Map[Y][X];
-//	for (auto i = 0; i < 4; ++i)
+//	for (auto i = 0; i < 4; ++i) //상 우 하 좌
 //	{
-//		int NY = Y + Dir[i][1];
+//		int NY = Y + Dir[i][1]; //다음index
 //		int NX = X + Dir[i][0];
-//		if (NX >= 0 && NY >= 0 && NX <= N && NY <= N)
+//		if (NX >= 0 && NY >= 0 && NX <= N && NY <= N) //칸검사
 //		{
-//			if (Map[NY][NX] > Now)
+//			if (Map[NY][NX] > Now)//그 방향의 값이 현재 index의 값보다 큰지
 //			{
 //				SideCheck = true;
-//				if (Num[NY][NX] != 0) {
-//					if (Count != 1)
-//						if (Num[Y][X]<1+Num[NY][NX])
+//				if (Num[NY][NX] != 0) //그 방향의 메모이제이션 배열이 0이 아니면(뭔가 지나온 값이 있다면)
+//				{
+//					if (Count != 1) 
+//					{
+//						if (Num[Y][X] < 1 + Num[NY][NX])
 //							Num[Y][X] = 1 + Num[NY][NX];
-//					else {
+//					}
+//					else 
+//					{
 //						int SetC = Count + Num[NY][NX];
 //						if (Num[_StartY][_StartX] < SetC)
 //							Num[_StartY][_StartX] = SetC;
