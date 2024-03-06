dafda928bb14766ea6abef0d2eb188221ee67dfe
동민 유 <vpt515@gmail.com>
2024-03-06 22:03:59 +0900

    유동민 : 백준15550 Update

---
+++ 백준15550(if2).cpp
@@ -0,0 +1,14 @@
+//#include <iostream>
+//using namespace std;
+//int main() {
+//    int a = 99999998 ;
+//    float b = 99999998 ;
+//    int c = 99999999 ;
+//    if (a == b && b == c && c != a) {
+//        cout << "true" << '\n';
+//    } 
+//    else {
+//        cout << "false" << '\n';
+//    }
+//    return 0;
+//}
