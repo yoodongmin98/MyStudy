eb20d6800acddc9a003ce5ed3cb11e6346d3cf17
yoodongmin98 <117648173+yoodongmin98@users.noreply.github.com>
2024-04-14 17:36:21 +0900

    유동민 :  백준14500 Update

--- 백준11723 (집합).cpp
+++ 백준11723 (집합).cpp
@@ -1,38 +1,38 @@
-#include <iostream>
-#include <string>
-using namespace std;
-
-int M;
-int S = 0;
-int main()
-{
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-	cin >> M;
-	for (auto i = 0; i < M; ++i)
-	{
-		string Oper;
-		int Num;
-		cin >> Oper;
-		if (Oper != "all" && Oper!="empty")
-			cin >> Num;
-		if (Oper == "add")
-			S = S | (1 << Num);
-		else if (Oper == "remove")
-			S = S & ~(1 << Num);
-		else if (Oper == "check")
-		{
-			if (S & (1 << Num))
-				cout << 1 << "\n";
-			else
-				cout << 0 << "\n";
-		}
-		else if (Oper == "toggle")
-			S = S ^ (1 << Num);
-		else if (Oper == "all")
-			S = 2147483647;
-		else if (Oper == "empty")
-			S = 0;
-	}
-}
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//
+//int M;
+//int S = 0;
+//int main()
+//{
+//	ios::sync_with_stdio(false);
+//	cin.tie(NULL);
+//	cout.tie(NULL);
+//	cin >> M;
+//	for (auto i = 0; i < M; ++i)
+//	{
+//		string Oper;
+//		int Num;
+//		cin >> Oper;
+//		if (Oper != "all" && Oper!="empty")
+//			cin >> Num;
+//		if (Oper == "add")
+//			S = S | (1 << Num);
+//		else if (Oper == "remove")
+//			S = S & ~(1 << Num);
+//		else if (Oper == "check")
+//		{
+//			if (S & (1 << Num))
+//				cout << 1 << "\n";
+//			else
+//				cout << 0 << "\n";
+//		}
+//		else if (Oper == "toggle")
+//			S = S ^ (1 << Num);
+//		else if (Oper == "all")
+//			S = 2147483647;
+//		else if (Oper == "empty")
+//			S = 0;
+//	}
+//}
---
+++ 백준14500(테트로미노).cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+
+int N, M;
+int Map[501][501];
+int MaxN = 0;
+int Dir[4][2] = { {0,-1}, {1,0}, {0,1}, {-1,0} };
+bool Isvisit[501][501] = { false };
+
+int Result = 0;
+void Solution(int Y,int X,int Re,int Depth)
+{
+	//내가 이거 잘못쳐서 몇번을 틀리니 ㅓ ㅗ ㅏ ㅜ
+	if (Y - 1 >= 0 && Y + 1 < N && X - 1 >= 0)
+		Result = max(Result, Map[Y - 1][X] + Map[Y + 1][X] + Map[Y][X] + Map[Y][X - 1]); //ㅓ
+	if (Y - 1 >= 0 && X + 1 < M && X - 1 >= 0)
+		Result = max(Result, Map[Y - 1][X] + Map[Y][X + 1] + Map[Y][X - 1] + Map[Y][X]);
+	if (Y - 1 >= 0 && Y + 1 < N && X + 1 < M)
+		Result = max(Result, Map[Y - 1][X] + Map[Y + 1][X] + Map[Y][X] + Map[Y][X + 1]);
+	if (Y + 1 < N && X + 1 < M && X - 1 >= 0)
+		Result = max(Result, Map[Y + 1][X] + Map[Y][X + 1] + Map[Y][X - 1] + Map[Y][X]);
+	Isvisit[Y][X] = true;
+	Re += Map[Y][X];
+	Result = max(Result,Re);
+	if (Depth == 4)
+	{
+		Isvisit[Y][X] = false;
+		return;
+	}	
+	for (auto k : Dir)
+	{
+		if (Y + k[1] >= 0 && Y + k[1] < N && X + k[0] >= 0 && X + k[0] < M)
+		{
+			int NY = Y + k[1];
+			int NX = X + k[0];
+			if(Isvisit[NY][NX]==false)
+				Solution(NY, NX, Re, Depth + 1);
+		}
+	}
+	Isvisit[Y][X] = false;
+}
+int main()
+{
+	cin >> N >> M;
+	for (auto i = 0; i < N; ++i)
+	{
+		for (auto j = 0; j < M; ++j)
+		{
+			int Num;
+			cin >> Num;
+			Map[i][j] = Num;
+		}
+	}
+	for (auto i = 0; i < N; ++i)
+	{
+		for (auto j = 0; j < M; ++j)
+		{
+			Solution(i, j, 0, 1);
+		}
+	}
+	cout << Result;
+}
