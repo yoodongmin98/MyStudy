d63314afd7f7e1570abbe8da04bf82a06d13a7b9
yoodongmin98 <117648173+yoodongmin98@users.noreply.github.com>
2024-07-23 17:57:23 +0900

    유동민 : 백준1799(비숍) 또 당했다.

--- 백준1799(비숍)(G1).cpp
+++ 백준1799(비숍)(G1).cpp
@@ -1,33 +1,35 @@
 //#include <iostream>
 //#include <vector>
 //#include <cmath>
+//#include <queue>
 //using namespace std;
 //int N;
 //vector<vector<bool>> Map;
 //int Set = 0;
-//int S1(vector<vector<bool>> _Map,int _Size)
+//priority_queue<int> MaxValue;
+//int S1(vector<vector<bool>> _Map, int _Size)
 //{
 //	int Result = 0;
-//	for (auto i = 0; i < _Size/2+1; ++i){
+//	for (auto i = 0; i < _Size / 2 + 1; ++i) {
 //		bool LC = false;
-//		for (auto j = 0; j < _Size; ++j){
-//			if (_Map[i][j] == true && false == LC){
+//		for (auto j = 0; j < _Size; ++j) {
+//			if (_Map[i][j] == true && false == LC) {
 //				LC = true;
 //				Result++;
-//				for (auto k = i; k < _Size; ++k){
-//					if(_Map[k][j]==true)
+//				for (auto k = i; k < _Size; ++k) {
+//					if (_Map[k][j] == true)
 //						_Map[k][j] = false;
 //				}
 //			}
 //		}
 //	}
-//	for (auto i = _Size-1; i >_Size/2; --i){
+//	for (auto i = _Size - 1; i > _Size / 2; --i) {
 //		bool LC = false;
-//		for (auto j = _Size-1; j >= 0; --j){
-//			if (_Map[i][j] == true && false == LC){  
+//		for (auto j = _Size - 1; j >= 0; --j) {
+//			if (_Map[i][j] == true && false == LC) {
 //				LC = true;
 //				Result++;
-//				for (auto k = i; k > _Size/2; --k) {
+//				for (auto k = i; k > _Size / 2; --k) {
 //					if (_Map[k][j] == true)
 //						_Map[k][j] = false;
 //				}
@@ -36,29 +38,91 @@
 //	}
 //	return Result;
 //}
-//int S2(vector<vector<bool>> _Map,int _Size)
+//int S3(vector<vector<bool>> _Map, int _Size)
 //{
 //	int Result = 0;
-//	for (auto i = _Size-1; i >= _Size / 2 ; --i) {
+//	for (auto i = 0; i < _Size / 2 + 1; ++i) {
 //		bool LC = false;
 //		for (auto j = 0; j < _Size; ++j) {
 //			if (_Map[i][j] == true && false == LC) {
 //				LC = true;
 //				Result++;
-//				for (auto k = i; k >=0; --k) {
+//				for (auto k = i; k < _Size; ++k) {
 //					if (_Map[k][j] == true)
 //						_Map[k][j] = false;
 //				}
 //			}
 //		}
 //	}
-//	for (auto i = 0; i <_Size/2; ++i) {
+//	for (auto i = _Size - 1; i > _Size / 2; --i) {
+//		bool LC = false;
+//		for (auto j = 0; j <= _Size - 1; ++j) {
+//			if (_Map[i][j] == true && false == LC) {
+//				LC = true;
+//				Result++;
+//				for (auto k = i; k > _Size / 2; --k) {
+//					if (_Map[k][j] == true)
+//						_Map[k][j] = false;
+//				}
+//			}
+//		}
+//	}
+//	return Result;
+//}
+//int S2(vector<vector<bool>> _Map, int _Size)
+//{
+//	int Result = 0;
+//	for (auto i = _Size - 1; i > _Size / 2; --i) {
+//		bool LC = false;
+//		for (auto j = 0; j < _Size; ++j) {
+//			if (_Map[i][j] == true && false == LC) {
+//				LC = true;
+//				Result++;
+//				for (auto k = i; k >= 0; --k) {
+//					if (_Map[k][j] == true)
+//						_Map[k][j] = false;
+//				}
+//			}
+//		}
+//	}
+//	for (auto i = 0; i < _Size / 2 + 1; ++i) {
 //		bool LC = false;
 //		for (auto j = _Size - 1; j >= 0; --j) {
 //			if (_Map[i][j] == true && false == LC) {
 //				LC = true;
 //				Result++;
-//				for (auto k = i; k < _Size / 2; ++k) {
+//				for (auto k = i; k <= _Size / 2; ++k) {
+//					if (_Map[k][j] == true)
+//						_Map[k][j] = false;
+//				}
+//			}
+//		}
+//	}
+//	return Result;
+//}
+//int S4(vector<vector<bool>> _Map, int _Size)
+//{
+//	int Result = 0;
+//	for (auto i = _Size - 1; i > _Size / 2; --i) {
+//		bool LC = false;
+//		for (auto j = 0; j < _Size; ++j) {
+//			if (_Map[i][j] == true && false == LC) {
+//				LC = true;
+//				Result++;
+//				for (auto k = i; k >= 0; --k) {
+//					if (_Map[k][j] == true)
+//						_Map[k][j] = false;
+//				}
+//			}
+//		}
+//	}
+//	for (auto i = 0; i < _Size / 2 + 1; ++i) {
+//		bool LC = false;
+//		for (auto j = 0; j <= _Size - 1; ++j) {
+//			if (_Map[i][j] == true && false == LC) {
+//				LC = true;
+//				Result++;
+//				for (auto k = i; k <= _Size / 2; ++k) {
 //					if (_Map[k][j] == true)
 //						_Map[k][j] = false;
 //				}
@@ -76,10 +140,10 @@
 //	{
 //		Map[p].resize(Size);
 //	}
-//	for (auto i = Size/2; i >= 0; --i)
+//	for (auto i = Size / 2; i >= 0; --i)
 //	{
 //		int Count = Set;
-//		for (auto j = i; j < i+N; ++j)
+//		for (auto j = i; j < i + N; ++j)
 //		{
 //			bool Possible;
 //			cin >> Possible;
@@ -88,7 +152,9 @@
 //		}
 //		Set++;
 //	}
-//	int T1=S1(Map,Size);
-//	int T2=S2(Map,Size);
-//	cout << max(T1,T2);
+//	MaxValue.push(S1(Map, Size));
+//	MaxValue.push(S2(Map, Size));
+//	MaxValue.push(S3(Map, Size));
+//	MaxValue.push(S4(Map, Size));
+//	cout << MaxValue.top();
 //}
--- 백준7576(토마토)-G5.cpp
+++ 백준7576(토마토)-G5.cpp
@@ -1,27 +1,28 @@
-#include <iostream>
+//#include <iostream>
+//
+//using namespace std;
+//
+//
+//int M, N;
+//int Map[1001][1001] = { -2, };
+//
+//void bfs(int y, int x, int day)
+//{
+//
+//}
+//
+//int main()
+//{
+//	cin >> M >> N;
+//	for (auto i = 0; i < N; ++i)
+//	{
+//		for (auto j = 0; j < M; ++j)
+//		{
+//			int Tomato;
+//			cin >> Tomato;
+//			Map[N][M] = Tomato;
+//		}
+//	}
+//	bfs(0,0,0);
+//}
 
-using namespace std;
-
-
-int M, N;
-int Map[1001][1001] = { -2, };
-
-void bfs(int y, int x, int day)
-{
-
-}
-
-int main()
-{
-	cin >> M >> N;
-	for (auto i = 0; i < N; ++i)
-	{
-		for (auto j = 0; j < M; ++j)
-		{
-			int Tomato;
-			cin >> Tomato;
-			Map[N][M] = Tomato;
-		}
-	}
-	
-}
