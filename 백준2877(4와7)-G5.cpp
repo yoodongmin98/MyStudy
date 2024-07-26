0786ccaa47d51cab34a7f3ef5b77795d061e026b
yoodongmin98 <117648173+yoodongmin98@users.noreply.github.com>
2024-07-26 13:07:13 +0900

    유동민 : 백준2877 Update

---
+++ 백준2877(4와7)-G5.cpp
@@ -0,0 +1,25 @@
+//#include <iostream>
+//#include <string>
+//#include <algorithm>
+//
+//using namespace std;
+//int k;
+//
+//int main()
+//{
+//    cin >> k;
+//    k++;
+//
+//    string result = "";
+//    while (k > 1) {
+//        if (k % 2 == 0) {
+//            result += '4';
+//        }
+//        else {
+//            result += '7';
+//        }
+//        k /= 2;
+//    }
+//    reverse(result.begin(), result.end());
+//    cout << result;
+//}
