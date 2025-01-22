255f7a5c415957281f4a52ed1bd05f39fa0ee21c
동민 유 <vpt515@gmail.com>
2025-01-22 09:21:00 +0900

    유동민 : ThreadTest Update

---
+++ thread.cpp
@@ -0,0 +1,44 @@
+//#include <iostream>
+//#include <vector>
+//#include <thread>
+//
+//
+//using namespace std;
+//
+//int ACount = 0;
+//int BCount = 0;
+////void A()
+////{
+////	//while (1)
+////	//{
+////		//cout << "ACount : " << ACount << endl;
+////		//ACount++;
+////	//}
+////}
+////void B()
+////{
+////	while (1)
+////	{
+////		for (auto i = 0; i < 4096; ++i)
+////		{
+//// 			cout << "BCount : " << BCount << endl;
+////		}
+////		BCount++;
+////	}
+////}
+//
+//
+//
+//
+//int main()
+//{
+//	int a = 16777216;
+//	float b = 16777217;
+//	int c = 16777217;
+//
+//	if (c == b)
+//		cout << "true";
+//	else
+//		cout << "false";
+//	
+//}
