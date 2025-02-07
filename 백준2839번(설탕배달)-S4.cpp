1d7bd8ffaf2699331b4f245078120ef7bd93fc6b
동민 유 <vpt515@gmail.com>
2025-02-07 13:12:48 +0900

    유동민 : 백준2839 Update

--- 백준2839번(설탕배달)-S4.cpp
+++ 백준2839번(설탕배달)-S4.cpp
@@ -1,42 +1,22 @@
 //#include <iostream>
 //
-//
-//
 //using namespace std;
 //
-// 
+//int main() {
+//    int N;
+//    cin >> N;
 //
-// 반례 : 3003일때 601이 아니라 1001이라고나옴(3먼저 나눈거 ㅇㅇ)
-// 
-//int N;
-//int Result = 0;
-//int main()
-//{
-//	cin >> N;
-//	while (1)
-//	{
-//		if (N % 5 == 0)
-//		{
-//			Result += N / 5;
-//			break;
-//		}
-//		if (N % 3 == 0)
-//		{
-//			Result += N / 3;
-//			break;
-//		}
+//    int result = -1; 
+//    for (int five = N / 5; five >= 0; five--) 
+//    {  
+//        int rem = N - (five * 5);  
+//        if (rem % 3 == 0) 
+//        { 
+//            int three = rem / 3;
+//            result = five + three;  
+//            break; 
+//        }
+//    }
 //
-//		if (N < 5)
-//			N -= 3;
-//		else
-//			N -= 5;
-//		Result++;
-//		if (N < 0)
-//		{
-//			Result = -1;
-//			break;
-//		}
-//		
-//	}
-//	cout << Result;
+//    cout << result;
 //}
